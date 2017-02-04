3dd0538bf8958cc2045b7ada7b900313ecd62067
Aerendir <hello@aerendir.me>
2017-02-04 15:35:32 +0100

    Make the package a bundle.

---
+++ src/ConsoleStylesBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SerendipityHQ\Bundle\ConsoleStyles;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * {@inheritdoc}
+ */
+class ConsoleStylesBundle extends Bundle
+{
+}
