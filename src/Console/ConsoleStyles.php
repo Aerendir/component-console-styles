d1fe60ced27f54f25d74de34fd0a161979faef0b
Aerendir <hello@aerendir.me>
2020-07-27 16:16:00 +0200

    Define constants for styles.

---
+++ src/Console/ConsoleStyles.php
@@ -0,0 +1,53 @@
+<?php
+namespace SerendipityHQ\Bundle\ConsoleStyles\Console;
+
+/**
+ * Constants that define styles available in CLI.
+ */
+class ConsoleStyles
+{
+    /** @var string */
+    public const CAUTION = 'caution';
+
+    /** @var string */
+    public const COMMENT = 'comment';
+
+    /** @var string */
+    public const ERROR = 'error';
+
+    /** @var string */
+    public const INFO = 'info';
+
+    /** @var string */
+    public const NOTE = 'note';
+
+    /** @var string */
+    public const SUCCESS = 'success';
+
+    /** @var string */
+    public const WARNING = 'warning';
+
+    /** @var string */
+    public const CAUTION_NOBG = 'caution-nobg';
+
+    /** @var string */
+    public const COMMENT_NOBG = 'comment-nobg';
+
+    /** @var string */
+    public const ERROR_NOBG = 'error-nobg';
+
+    /** @var string */
+    public const INFO_NOBG = 'info-nobg';
+
+    /** @var string */
+    public const NOTE_NOBG = 'note-nobg';
+
+    /** @var string */
+    public const QUESTION_NOBG = 'question-nobg';
+
+    /** @var string */
+    public const SUCCESS_NOBG = 'success-nobg';
+
+    /** @var string */
+    public const WARNING_NOBG = 'warning-nobg';
+}
