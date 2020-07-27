7e5fa15232b075e4a5c3b85b51b54d4d0dba2981
Aerendir <hello@aerendir.me>
2020-07-27 16:15:45 +0200

    Define constants for colors.

---
+++ src/Console/ConsoleColors.php
@@ -0,0 +1,32 @@
+<?php
+namespace SerendipityHQ\Bundle\ConsoleStyles\Console;
+
+/**
+ * Constants that define colors available in CLI.
+ */
+class ConsoleColors
+{
+    /** @var string */
+    public const BLACK = 'black';
+
+    /** @var string */
+    public const BLUE = 'blue';
+
+    /** @var string */
+    public const CYAN = 'cyan';
+
+    /** @var string */
+    public const GREEN = 'green';
+
+    /** @var string */
+    public const YELLOW = 'yellow';
+
+    /** @var string */
+    public const MAGENTA = 'magenta';
+
+    /** @var string */
+    public const RED = 'red';
+
+    /** @var string */
+    public const WHITE = 'white';
+}
